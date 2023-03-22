6234de1b722cee3311402d5588a0d3e44e00f6c6
Kadu17 <118760023+Kadu17@users.noreply.github.com>
2023-03-22 11:24:27 -0300

    Update Main.java

--- Tabuada/src/Main.java
+++ Tabuada/src/Main.java
@@ -9,5 +9,12 @@ public class Main {
          for (int n = 1; n <= 10; n++){
              System.out.println(entrada + "x" + n + " = " + n*entrada);
          }
+        
+        //todas as tabuadas uma na frente da outra\\
+         for (int i = 1; i < 11 ; i++) {
+            for (int j = 1; j < 11; j++) {
+                System.out.printf("%d * %d = %d             ", i, j, i*j);
+            }
+            System.out.println();
     }
 }
