e36d48da9e53c9070b8eee39c53759590e79994b
Kadu17 <118760023+Kadu17@users.noreply.github.com>
2023-03-22 11:31:17 -0300

    Add files via upload

---
+++ Wrappers.java
@@ -0,0 +1,118 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Wrappers {
+    public static void main(String[] args){
+
+//        Byte b = 100;
+//        Short s = 1000;
+//        Integer i = 10000;
+//        Long l = 1000000L;
+//
+//        System.out.println(b.byteValue());
+//        System.out.println(s.toString());
+//        System.out.println(i*3);
+//        System.out.println(l/3);
+
+//        int a = 10;
+//        Integer b = 10;
+//        ArrayList<Integer> lista = new ArrayList<>();
+//        lista.add(10);
+//        System.out.println(lista);
+
+//        Float f = 123.10F;
+//        System.out.println(f);
+//
+//        Double d = 1234.5678;
+//        System.out.println(d);
+//
+//        Boolean bo = Boolean.parseBoolean("true");
+//        System.out.println(bo);
+//        System.out.println(bo.toString().toUpperCase());
+
+//        double a =1;
+//        System.out.println(a);
+//
+//        float b = (float) 1.22244499999;
+//        System.out.println(b);
+
+//        int c= 127;
+//        byte d = (byte) c;
+//        System.out.println(d);
+
+//        double e = 1.999999999;
+//        int f =(int) e;
+//        System.out.println(f);
+
+//        Integer num1 = 10000;
+//        System.out.println(num1.toString().length());
+//
+//        int num2 = 1000000;
+//        System.out.println(Integer.toString(num2).length());
+//
+//        System.out.println(("" + num2).length());
+
+//        String numero1 = "12";
+//        String numero2 = "3.14";
+//
+//        int x = Integer.parseInt(numero1);
+//        double y = Double.parseDouble(numero2);
+//
+//        // Printa qual o tipo da variavel,se é string, int...
+//        System.out.println(((Object)x).getClass().getSimpleName());
+//        System.out.println(((Object)y).getClass().getSimpleName());
+
+//        int contador = 0;
+//        while (contador <= 20){
+//            try { Thread.sleep (1000); } catch (InterruptedException ex) {}
+//            String frase = "O contador está em "+contador+" \n";
+//            System.out.println(frase);
+//            contador++;
+//        }
+//
+//        for (int i = 0; i <= 50; i++){
+//            System.out.print("=");
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        int contador = 0;
+//        int acumulador = 0;
+//
+//        for (int i = 10; i <= 0; i++) {
+//            System.out.println(i);
+//        }
+
+//        for (int i = 0; i <5 ; i++) {
+//            contador=i;
+//            acumulador+=i*i;
+//            System.out.printf("O contador esta em %d e" +  " o acumulador esta em %d\n",contador,acumulador);
+//        }
+
+//        while (contador < 5) {
+//            contador++;
+//            System.out.print("Digite um número: ");
+//            acumulador = acumulador+sc.nextInt();
+//            sc.nextLine();
+//            System.out.printf("O contador está em %d o acumulador está em %d\n",contador,acumulador);
+//        }
+//        for (int i = 1; i <10 ; i++) {
+//            if (i%2 == 0){
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Almoço");
+//            for (int j = 0; j < 3; j++) {
+//                System.out.println("Janta");
+//            }
+//        }
+//        for (int i = 1; i < 11 ; i++) {
+//            for (int j = 1; j < 11; j++) {
+//                System.out.printf("%d * %d = %d             ", i, j, i*j);
+//            }
+//            System.out.println();
+//
+//        }
+    }
+}
