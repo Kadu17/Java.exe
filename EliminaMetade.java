2ac5765d4ed3806d3eba44fd0ddbb8b91b3d4ed5
Kadu17 <118760023+Kadu17@users.noreply.github.com>
2023-05-24 09:03:59 -0300

    EliminaMetade.java

--- EliminaMetade.java
+++ EliminaMetade.java
@@ -2,11 +2,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
-public class EliminaMetade {
-    ArrayList<String> listaDeNomes = new ArrayList<String> (Arrays.asList("Allana", "Angelo","Carlos","Eduarda", "Giovanna", "Geovana", "Giovana","Laura", "Felipe",
-            "Diego", "Diegão", "Nicole","Henrique","Esther","Caio","Julia","Lucas","Bona"));
+class EliminarMetade {
 
+    public static void main(String[] args) {
+        ArrayList<String> listaDeNomes = new ArrayList<>(Arrays.asList("Alana", "Angelo", "Caio", "Kadu", "Diego Castan",
+                "Diego Alencar", "Eduarda", "Esther", "Felipe", "Laura", "Lucas", "Gabriel Bonaretti", "Giovanna Fiuza", "Giovana Souza",
+                "Giovana Radaeli", "Henrique", "Julia", "Nicole"));
 
+        Random rand = new Random();
 
+        int tamanhoLista = listaDeNomes.size();
 
+        if (tamanhoLista % 2 == 0) {
+            for (int i = 0; i < (tamanhoLista / 2); i++) {
+                listaDeNomes.remove(rand.nextInt(listaDeNomes.size()));
+            }
+            System.out.println(listaDeNomes);
+        } else {
+            for (int i = 0; i < ((tamanhoLista + 1) / 2); i++) {
+                listaDeNomes.remove(rand.nextInt(listaDeNomes.size()));
+            }
+            System.out.println(listaDeNomes);
+
+        }
+    }
 }
